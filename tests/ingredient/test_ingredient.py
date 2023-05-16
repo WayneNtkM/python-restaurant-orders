fa4db3b42874c2035ab3cd131dfff102acc2e6c5
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-05-16 11:47:27 -0300

    test: test for Ingredient Class implemented

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    flour = Ingredient("farinha")
+
+    assert isinstance(flour, Ingredient)
+    assert flour.name == "farinha"
+    assert flour.__eq__(Ingredient("carne")) is False
+    assert flour.__eq__(Ingredient("farinha"))
+    assert flour.__repr__() == "Ingredient('farinha')"
+    assert hash(flour) == hash(Ingredient("farinha"))
+    assert hash(flour) != hash(Ingredient("carne"))
+    assert flour.restrictions == {Restriction.GLUTEN}
